8f1dfe9de54302421d88d24c18b9aead2e516d20
IamFJ03 <130732739+IamFJ03@users.noreply.github.com>
2025-10-27 16:58:59 +0530

    added Testing video

--- frontend/Pages/Home.jsx
+++ frontend/Pages/Home.jsx
@@ -1,12 +1,43 @@
-import React,{useCallback, useEffect, useState} from 'react';
+import React,{useCallback, useEffect, useRef, useState} from 'react';
 import Sidebar from '../components/sidebar';
-import { useSocket } from '../Provider/Socket'
-import { useNavigate} from 'react-router-dom'
+import { useSocket } from '../Provider/Socket';
+import { useNavigate} from 'react-router-dom';
+
 export default function Home() {
   const [email, setEmail] = useState("");
   const [roomId, setRoomId] = useState("");
-  const {socket} = useSocket()
-  const navigate = useNavigate()
+  const [myStream, setMyStream] = useState(null);
+  const videoRef = useRef(null);
+
+  const {socket} = useSocket();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+     let currentStream = null;
+     const fetchStream = async () => {
+      try{
+        const stream = await navigator.mediaDevices.getUserMedia({
+          audio: true,
+          video: true
+        });
+        currentStream = stream;
+
+        if(videoRef.current)
+          videoRef.current.srcObject = currentStream
+      }
+      catch(e){
+console.error("Error accessing media devices:", e);
+      }
+     } 
+
+     fetchStream();
+
+     return () => {
+      if(currentStream)
+        currentStream.getTracks().forEach(track => track.stop());
+     };
+  },[]);
+
   const handleJoinRoom = () => {
     socket.emit('join-room',{email, roomId})
   }
@@ -27,6 +58,13 @@ export default function Home() {
     <div>
       <div className='flex'>
       <Sidebar />
+      <video
+autoPlay
+playsInline
+muted
+ref={videoRef}
+className='w-100 h-100 border-1 mt-30 rounded shadow-xl'
+      />
       <div className='mt-30 ml-50 shadow-2xl h-100 p-10 rounded'>
       <p className='font-bold text-3xl my-5'>Join Meeting</p>
       <input type='email' value={email} onChange={e=> setEmail(e.target.value)} placeholder='Enter Email' className='border-1 p-2 px-5 rounded w-100'></input><br /><br/>
