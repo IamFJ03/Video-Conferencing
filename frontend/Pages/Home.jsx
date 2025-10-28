5dcbb5219c4b2188685953e79efdbe74c0ec6826
IamFJ03 <130732739+IamFJ03@users.noreply.github.com>
2025-10-28 12:06:54 +0530

    Improved Version

--- frontend/Pages/Home.jsx
+++ frontend/Pages/Home.jsx
@@ -1,11 +1,16 @@
 import React,{useCallback, useEffect, useRef, useState} from 'react';
 import Sidebar from '../components/sidebar';
 import { useSocket } from '../Provider/Socket';
+import { useUserContext } from '../Provider/UserContext';
 import { useNavigate} from 'react-router-dom';
+import { Video, Mic, VideoOff, MicOff } from 'lucide-react';
 
 export default function Home() {
+  const { user } = useUserContext();
   const [email, setEmail] = useState("");
   const [roomId, setRoomId] = useState("");
+  const [microphone, setMicrophone] = useState(true);
+  const [vid, setVid] = useState(true);
   const [myStream, setMyStream] = useState(null);
   const videoRef = useRef(null);
 
@@ -57,38 +62,58 @@ const handleCamera = useCallback(() => {
     const videoTrack = myStream.getVideoTracks()[0];
     if(videoTrack){
       videoTrack.enabled = !videoTrack.enabled;
+      setVid(!vid);
     }
   }
-},[myStream])
+},[myStream, vid])
 
 const handleMicrophone = useCallback(() => {
     if(myStream){
       const audioTrack = myStream.getAudioTracks()[0];
       if(audioTrack){
         audioTrack.enabled = !audioTrack.enabled
+        setMicrophone(!microphone);
       }
     }
-},[myStream])
+},[myStream, microphone])
 
   return (
     
     <div>
       <div className='flex'>
       <Sidebar />
+      <div>
       <video
 autoPlay
 playsInline
 muted
 ref={videoRef}
-className='w-100 h-80 mt-40 rounded shadow-xl'
+className='w-100 h-80 mt-30 rounded shadow-xl'
       />
+      <div className='flex justify-between items-center px-25 mt-10'>
+        <div className='bg-white p-2 rounded-full shadow-xl'>
+          {vid ? <Video size={35} color='black' />
+          :
+          <VideoOff size={35} color='black' />
+          }
+      </div>
+      <div className='bg-white p-2 rounded-full shadow-xl'>
+      {
+        microphone ?
+        <Mic size={35} color='black' />
+        :
+        <MicOff size={35} color='black' />
+      }
+      </div>
+      </div>
+      </div>
       <div className='mt-30 ml-50 shadow-2xl h-100 p-10 rounded'>
       <p className='font-bold text-3xl my-5'>Join Meeting</p>
-      <input type='email' value={email} onChange={e=> setEmail(e.target.value)} placeholder='Enter Email' className='border-1 p-2 px-5 rounded w-100'></input><br /><br/>
+      <input type='email' value={user.email ? user.email : 'Login Required...'}  className='border-1 p-2 px-5 rounded w-100 text-gray-500' disabled ></input><br /><br/>
       <input type='text' value={roomId} onChange={e=> setRoomId(e.target.value)} placeholder='Enter Room Id' className='border-1 p-2 px-5  rounded w-100'></input><br /><br/>
       <button type='button' className='px-15 py-3 bg-black text-white rounded hover:cursor-pointer hover:border-1' onClick={handleJoinRoom}>Join Room</button>
-      <p className='text-left font-bold text-gray-600 my-5'>Turn Off Microphone<input type='checkbox' onClick={handleMicrophone} className='absolute right-30 h-4 w-4'></input> </p>
-      <p className='text-left font-bold text-gray-600'>Turn Off Camera<input type='checkbox' onClick={handleCamera} className='absolute right-30 h-4 w-4'></input></p>
+      <p className='text-left font-bold text-gray-600 my-5'>Turn On Microphone<input type='checkbox' checked={microphone} onClick={handleMicrophone} className='absolute right-50 h-4 w-4'></input> </p>
+      <p className='text-left font-bold text-gray-600'>Turn On Camera<input type='checkbox' checked={vid} onClick={handleCamera} className='absolute right-50 h-4 w-4'></input></p>
       </div>
       </div>
     </div>
